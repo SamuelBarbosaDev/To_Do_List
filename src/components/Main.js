13abc40be5de857f828c66d068979298ef37b4ba
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-09-12 16:47:49 -0300

    feat: Trabalhando nas tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
 import './Main.css'
+import React, { Component } from 'react';
+import { FaPlus } from 'react-icons/fa'
+import { FaEdit, FaWindowClose } from 'react-icons/fa'
+
+
 export default class Main extends Component {
     state = {
-        novaTarefa: ''
+        novaTarefa: '',
+        tarefas: [
+            'Estudar Javascript',
+            'Treinar',
+            'Ler a Bíblia',
+            'Estudar Espanhol',
+            'Fazer alongamentos',
+            'Fazer Barras',
+            'Elevação de pernas na barra'
+        ]
     };
 
     handleChange = (e) =>{
@@ -11,17 +24,28 @@ export default class Main extends Component {
         });
     }
     render(){
-        const { novaTarefa } = this.state;
+        const { novaTarefa, tarefas } = this.state;
 
         return (
             <main>
                 <section className='main-content'>
                     <h1>{ (!novaTarefa && 'Lista de Tarefas') || (novaTarefa)}</h1>
 
-                    <form action='#'>
+                    <form action='#' className='form'>
                         <input onChange={this.handleChange} type='text' />
-                        <button type='submit'>Enviar</button>
+                        <button type='submit'><FaPlus /></button>
                     </form>
+                    <ul className='tasks'>
+                        {tarefas.map(tarefa => (
+                            <li key={tarefa}>
+                                {tarefa}
+                                <div className='task-buttons'>
+                                    <FaEdit className='task-button-edit'/>
+                                    <FaWindowClose className='task-button-delete'/>
+                                </div>
+                            </li>
+                        )) }
+                    </ul>
                 </section>
             </main>
         )
